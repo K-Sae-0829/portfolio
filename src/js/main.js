abc8814d626b975982e31b9280e63510c4c11fc0
SPC-Kimura-S <kimura-s@spc-jpn.co.jp>
2022-12-02 21:19:54 +0900

    fix: ブートストラップ試み

--- src/js/main.js
+++ src/js/main.js
@@ -3,32 +3,7 @@
 // import 'core-js/features/number/is-nan' // SwiperのIE対応
 // import 'custom-event-polyfill' // SwiperのIE対応
 import { Autoplay, Controller, EffectFade, Navigation, Pagination, Swiper } from 'swiper'
-Swiper.use([
-  Autoplay,
-  Controller,
-  EffectFade,
-  Navigation,
-  Pagination,
-  A11y,
-  Autoplay,
-  Controller,
-  EffectCoverflow,
-  EffectCube,
-  EffectFade,
-  EffectFlip,
-  HashNavigation,
-  History,
-  Keyboard,
-  Lazy,
-  Mousewheel,
-  Navigation,
-  Pagination,
-  Parallax,
-  Scrollbar,
-  Thumbs,
-  Virtual,
-  Zoom,
-]) // Swiper宣言
+Swiper.use([Autoplay, Controller, EffectFade, Navigation, Pagination, A11y, Autoplay]) // Swiper宣言
 // その他、Swiperで使う機能があったら上に追加で宣言する
 // A11y, Autoplay, Controller, EffectCoverflow, EffectCube, EffectFade, EffectFlip, HashNavigation, History, Keyboard, Lazy, Mousewheel, Navigation, Pagination, Parallax, Scrollbar, Thumbs, Virtual, Zoom,
 $.scrollify({
--- src/page-works.php
+++ src/page-works.php
@@ -8,21 +8,80 @@
         <div class="works__search">
             <p class="search__ttl">絞り込み<i class="icon icon-down search__down"></i></p>
         </div>
-        <div class="works__list">
-            <a class="worksItem">
-                <div class="worksItem__thumb">
-                    <p class="worksItem__cat">WEB</p>
-                    <div class="worksItem__img">
-                        <img src="<?php echo T_URL; ?>img/site-thumb.png" alt="">
-                    </div>
-                    <div class="worksItem__txt-area">
-                        <div class="worksItem__tag-area">
-                            <p class="worksItem__tag">デザイン</p>
+        <div class="container">
+            <div class="works__list row">
+                <a class="worksItem col-lg-4">
+                    <div class="worksItem__thumb">
+                        <p class="worksItem__cat">WEB</p>
+                        <div class="worksItem__img">
+                            <img src="<?php echo T_URL; ?>img/site-thumb.png" alt="">
                         </div>
-                        <p class="worksItem__ttl">リスクゼロ.com</p>
+                        <div class="worksItem__txt-area">
+                            <div class="worksItem__tag-area">
+                                <p class="worksItem__tag">デザイン</p>
+                            </div>
+                            <p class="worksItem__ttl">リスクゼロ.com</p>
+                        </div>
                     </div>
-                </div>
-            </a>
+                </a>
+                <a class="worksItem col-lg-4">
+                    <div class="worksItem__thumb">
+                        <p class="worksItem__cat">WEB</p>
+                        <div class="worksItem__img">
+                            <img src="<?php echo T_URL; ?>img/site-thumb.png" alt="">
+                        </div>
+                        <div class="worksItem__txt-area">
+                            <div class="worksItem__tag-area">
+                                <p class="worksItem__tag">デザイン</p>
+                            </div>
+                            <p class="worksItem__ttl">リスクゼロ.com</p>
+                        </div>
+                    </div>
+                </a>
+                <a class="worksItem col-lg-4">
+                    <div class="worksItem__thumb">
+                        <p class="worksItem__cat">WEB</p>
+                        <div class="worksItem__img">
+                            <img src="<?php echo T_URL; ?>img/site-thumb.png" alt="">
+                        </div>
+                        <div class="worksItem__txt-area">
+                            <div class="worksItem__tag-area">
+                                <p class="worksItem__tag">デザイン</p>
+                            </div>
+                            <p class="worksItem__ttl">リスクゼロ.com</p>
+                        </div>
+                    </div>
+                </a>
+                <a class="worksItem col-lg-4">
+                    <div class="worksItem__thumb">
+                        <p class="worksItem__cat">WEB</p>
+                        <div class="worksItem__img">
+                            <img src="<?php echo T_URL; ?>img/site-thumb.png" alt="">
+                        </div>
+                        <div class="worksItem__txt-area">
+                            <div class="worksItem__tag-area">
+                                <p class="worksItem__tag">デザイン</p>
+                            </div>
+                            <p class="worksItem__ttl">リスクゼロ.com</p>
+                        </div>
+                    </div>
+                </a>
+                <a class="worksItem col-lg-4">
+                    <div class="worksItem__thumb">
+                        <p class="worksItem__cat">WEB</p>
+                        <div class="worksItem__img">
+                            <img src="<?php echo T_URL; ?>img/site-thumb.png" alt="">
+                        </div>
+                        <div class="worksItem__txt-area">
+                            <div class="worksItem__tag-area">
+                                <p class="worksItem__tag">デザイン</p>
+                            </div>
+                            <p class="worksItem__ttl">リスクゼロ.com</p>
+                        </div>
+                    </div>
+                </a>
+
+            </div>
         </div>
     </section>
 </article>
